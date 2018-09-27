9765a36a033fb0477e8907742ca82c56aef41248
YusupN <43654288+YusupN@users.noreply.github.com>
2018-09-27 22:49:54 +0300

    Create spamfilter.h

---
+++ spamfilter.h
@@ -0,0 +1,21 @@
+#ifndef SPAM_FILTER_H
+#define SPAM_FILTER_H
+#include<vector>
+#include"word.h"
+
+using namespace std;
+
+class spam_filter
+{
+private:
+    vector<word*> wordlist;  //последовательность классов, внутри которых содержатся 
+    //слова и то как часто эти слова встречались в спаме
+public:
+    spam_filter();
+    void count_test_spam(); //функция обучения на тестовом спаме
+    void count_test_ham();  //обучение на тестовом не спамовом письме
+    double check_mail();      //проверка письма на спамовость
+    void show_datas();       //
+};
+
+#endif // SPAM_FILTER_H

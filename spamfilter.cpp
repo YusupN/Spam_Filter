2d4db9464edf8d68a784b7a881d22cca565fbe9a
YusupN <43654288+YusupN@users.noreply.github.com>
2018-09-27 23:18:52 +0300

    Update spamfilter.cpp

--- spamfilter.cpp
+++ spamfilter.cpp
@@ -29,7 +29,8 @@ spam_filter::spam_filter()
                     wordlist[i]->common_counter();//то просто увеличивает счетчик
                     i=wordlist.size()+1;         //и выходит из цикла
                 }
-                else if ( (!wordlist[i]->compare_word(str)) && (i==wordlist.size()) ) //если дошел до конца вектора не найдя совпадений
+                else if ( (!wordlist[i]->compare_word(str)) && (i==wordlist.size()) ) //если дошел до конца вектора 
+                    //не найдя совпадений
                 {
                     word *w= new word;     //то создает новый класс
                     w->set_word(str);       //записывает новое слово
@@ -80,31 +81,41 @@ spam_filter::spam_filter()
     {
         string str;
         ifstream mail("file.txt"); //открывает файл письма
-        vector<string> mw;
+        
+        vector<string> mw;           //все из письма перелагаем в вектор стрингов.
         while (getline(mail, str))
         {
             mw.push_back(str);
         }
-        double a=1;
-        double b=1;
+        
+        
+        //Дальше вычисление спамовости по формуле опубликованной Грэхамом
+        //p=(p1*p2*...*pn)/( (p1*p2*...*pn)+( (1-p1)*(1-p2)*...*(1-pn) ) )
+        
+        double a=1;              //  a=p1*p2*....*pn где спамовость к-того слова из письма
+        double b=1;              //  b=(1-p1)*(1-p2)*...*(1-pn)
         for (int k=0; k<mw.size(); ++k)
         {
             for (int i=0; i<wordlist.size(); ++i)
             {
-                if ( wordlist[i]->compare_word(mw[i]) )
+                if ( wordlist[i]->compare_word(mw[i]) )//ищем в нашем контейнере совпадения с данными словами
                 {
-                    a=a*wordlist[i]->give_spamity();
-                    b=b*(1-wordlist[i]->give_spamity());
-                    i=wordlist.size()+1;
+                    a=a*wordlist[i]->give_spamity();       //если находим выдаем значение спамовости 
+                    b=b*(1-wordlist[i]->give_spamity());    //
+                    i=wordlist.size()+1;                   //и выходим из цикла
                 }
-                else if ( (!wordlist[i]->compare_word(mw[i])) && (i==wordlist.size()) )
+                else if ( (!wordlist[i]->compare_word(mw[i])) && (i==wordlist.size()) )//если дошли до конца списка и не нашли
                 {
+                    //значит эти слва не встречались ранее считаем их спамовость за 0.5
                     a=a*0.5;
                     b=b*0.5;
                 }
             }
         }
         double mail_spamity=a/(a+b);
+        
+        
+        //после того как фильтр вычислил спамовсть слова он может начать самообучение за счет этого письма
 
         for (int k=0; k<mw.size(); ++k)
         {
